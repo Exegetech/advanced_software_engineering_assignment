8e5de91d58c54a693c585ce7c6f70f7f0d2abfda
Exegetech <exegetech@protonmail.ch>
2023-06-23 10:06:10 +0700

    Second commit

--- unit4/kwic_refactored.py
+++ unit4/kwic_refactored.py
@@ -6,7 +6,13 @@ class IndexWordPair:
     def __init__(self, index: int, word: str):
         self.index = index
         self.word = word
+
+    def get_index(self) -> int:
+        return self.index
     
+    def get_word(self) -> str:
+        return self.word
+
 
 class Line:
     def __init__(self, words: list[str]):
@@ -17,16 +23,31 @@ class Line:
         return self
 
     def __next__(self) -> IndexWordPair:
-        index = self.index
-        word = self.storage[index]
-        self.index += 1
-        return IndexWordPair(index, word)
+        if self.index < len(self.storage):
+            index = self.index
+            word = self.storage[index]
+            self.index += 1
+            return IndexWordPair(index, word)
+        else:
+            raise StopIteration
+
+    def get_size(self) -> int:
+        return len(self.storage)
+
+    def get_word(self, index: int) -> str:
+        return self.storage[index]
 
 
 class IndexLinePair:
     def __init__(self, index: int, line: Line):
         self.index = index
         self.line = line
+
+    def get_index(self) -> int:
+        return self.index
+    
+    def get_line(self) -> Line:
+        return self.line
 
 
 class LineStorage:
@@ -43,80 +64,94 @@ class LineStorage:
         return self
 
     def __next__(self) -> IndexLinePair:
-        index = self.index
-        line = self.storage[index]
-        self.index += 1
-        return IndexLinePair(index, line)
+        if self.index < len(self.storage):
+            index = self.index
+            line = self.storage[index]
+            self.index += 1
+            return IndexLinePair(index, line)
+        else:
+            raise StopIteration
+
+    def get_line(self, index: int) -> Line:
+        return self.storage[index]
 
     
-line_storage = None
-def putfile(linelist):
+line_storage = LineStorage()
+
+
+def putfile(linelist: list[str]) -> None:
     global line_storage
-    line_storage = copy.copy(linelist)
+    line_storage.add_lines(linelist)
+
 
 circ_index = None
 
 
-def cs_setup():
+def cs_setup() -> None:
     global circ_index, line_storage
     
     circ_index = []
-    for lineno in range(len(line_storage)):
-        line = line_storage[lineno]
-        for wordno in range(len(line)):
-            circ_index.append( (lineno, wordno) )
-            
-######################################################################
-## ALPHABETIZING MODULE
+    for idx_line_pair in line_storage:
+        line_idx = idx_line_pair.get_index()
+        line_obj = idx_line_pair.get_line()
 
+        for idx_word_pair in line_obj:
+            word_idx = idx_word_pair.get_index()
+            circ_index.append((line_idx, word_idx))
+
+            
 alph_index = None
 
-def alphabetize():
+
+def alphabetize() -> None:
     global alph_index, line_storage, circ_index
-    def cmp_csline(shift1, shift2):
-      def csword(shift, wordno, lines):
+
+    def cmp_csline(shift1, shift2) -> bool:
+      def csword(shift, wordno: int, lines: LineStorage) -> str:
         (lno, first_word_no) = shift
-        shift_idx = (first_word_no + wordno) % len(lines[lno])
-        return lines[lno][shift_idx]
+        line = lines.get_line(lno)
+        line_size = line.get_size()
+        shift_idx = ((first_word_no + wordno) % line_size)
+        return line.get_word(shift_idx)
     
-      def cswords(shift, lines):
-        return len(lines[shift[0]])
+      def cswords(shift, lines: LineStorage) -> int:
+        line_idx = shift[0]
+        line = lines.get_line(line_idx)
+        return line.get_size()
     
-      def cmp(num1, num2):
+      def cmp(num1: int, num2: int) -> bool:
         return (num1>num2)-(num1<num2)
     
       lines = line_storage
-      
+          
       nwords1 = cswords(shift1, lines)
       nwords2 = cswords(shift2, lines)
       lasti = min(nwords1, nwords2)
-      
+          
       for i in range(lasti+1):
         cword1 = csword(shift1, i, lines)
         cword2 = csword(shift2, i, lines)
-        
+          
         if cword1 != cword2:
           return cmp(cword1, cword2)
-      
+          
       return cmp(nwords1, nwords2)
   
     alph_index = sorted(circ_index, key=functools.cmp_to_key(cmp_csline))
     
-######################################################################
-## OUTPUT MODULE
-
-def print_all_alph_cs_lines():
+def print_all_alph_cs_lines() -> None:
     global alph_index, line_storage
-    def csline(shift, lines):
+
+    def csline(shift, lines) -> list[str]:
         (lno, first_word_no) = shift
-        wrd_cnt = len(lines[lno])
-        return [lines[lno][(i+first_word_no) % wrd_cnt] for i in range(wrd_cnt)]
+        line = lines.get_line(lno)
+        wrd_cnt = line.get_size()
+        return [line.get_word((i+first_word_no) % wrd_cnt) for i in range(wrd_cnt)]
     
     for shift in alph_index:
-        print (csline(shift, line_storage))
+        print(csline(shift, line_storage))
 
         
-## MASTER CONTROL
 putfile([["a", "b", "c", "d"],
          ["one"],
          ["hey", "this", "is", "different"],
